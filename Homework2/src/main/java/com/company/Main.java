7622564ce71f663f9b4dcab4dcfce459d1c38f7a
Orest Shemelyuk <orestshemelyuk@gmail.com>
2019-04-01 17:29:53 +0300

    added files

---
+++ Homework2/src/main/java/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import org.junit.Assert;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Orest Shemelyuk");
+        Assert.fail("test failed");
+    }
+}
